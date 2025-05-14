b9de27b234c8db37e1ce7b42eaa586dddf5a902f
Dejan Mironski Panchevski <99611699+DekiDejan@users.noreply.github.com>
2025-05-14 20:04:36 +0200

    opdate gitignore

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-357bf293babb87c5.js","static/chunks/fd9d1056-9f91b5e418130764.js","static/chunks/117-e44c88db53920bf9.js","static/chunks/main-app-4a9eae4f0a70ce08.js"],pages:{"/_app":["static/chunks/webpack-357bf293babb87c5.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-12d29e3a9c0e9bf0.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-357bf293babb87c5.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-12d29e3a9c0e9bf0.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
